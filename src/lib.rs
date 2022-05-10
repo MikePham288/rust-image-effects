30d3491efa6ec81ea17e98ad6a29a7d68b2cda38
Mike <Mikephse@gmail.com>
2022-05-10 01:21:01 -0400

    Added loading image from memory on Rust side

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,15 @@
 use wasm_bindgen::prelude::*;
 use web_sys::console::log_1 as log;
 use base64::decode;
+use image::load_from_memory;
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
+
 
 #[wasm_bindgen]
 pub fn grayScale(encoded_file: &str) {
     log(&"Grayscale method called".into());
     let base64_to_vector = decode(encoded_file).unwrap();
     log(&"image decoded in Rust".into());
+    let img = load_from_memory(&base64_to_vector).unwrap();
+    log(&"image loaded".into());
 }
