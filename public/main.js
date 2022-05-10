375abeb0c0e49809c84c367970cce32b5f248385
Mike <Mikephse@gmail.com>
2022-05-10 01:12:04 -0400

    Added decoding base64 image from JavaScript to Rust

--- public/main.js
+++ public/main.js
@@ -17,8 +17,6 @@ async function init() {
       ""
     );
     rustApp.grayScale(base64);
-    console.log(input.files[0]);
-    console.log("base 64 of an image: ", base64);
   };
   input.addEventListener("change", () => {
     fireReader.readAsDataURL(input?.files[0]);
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+use wasm_bindgen::prelude::*;
+use web_sys::console::log_1 as log;
+use base64::decode;
+
 #[cfg(test)]
 mod tests {
     #[test]
@@ -5,4 +9,11 @@ mod tests {
         let result = 2 + 2;
         assert_eq!(result, 4);
     }
+}
+
+#[wasm_bindgen]
+pub fn grayScale(encoded_file: &str) {
+    log(&"Grayscale method called".into());
+    let base64_to_vector = decode(encoded_file).unwrap();
+    log(&"image decoded in Rust".into());
 }
