fc9443e32a324a898998879721945dc4529b445e
Mike <Mikephse@gmail.com>
2022-05-10 00:43:49 -0400

    add convert image to base 64 method on javascript side

--- public/main.js
+++ public/main.js
@@ -1,1 +1,20 @@
-console.log('test');
+console.log("test");
+
+function init() {
+  const input = document.getElementById("upload");
+  const fireReader = new FileReader();
+  fireReader.onloadend = () => {
+    const base64 = fireReader.result.replace(
+      /^data:image\/(png|jpeg|jpg);base64,/,
+      ""
+    );
+    console.log(input.files[0]);
+    console.log("base 64 of an image: ", base64);
+  };
+  input.addEventListener("change", () => {
+    fireReader.readAsDataURL(input?.files[0]);
+  });
+}
+
+init();
+
